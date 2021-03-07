1c6de75edd068c612408e099ffd6e761a33d2f3b
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-03-07 12:44:52 -0700

    removed requirements in server file

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 // Requiring everything at the top of the file
 const inquirer = require("inquirer");
 const db = require("./db");
-const logo = require("asciiart-logo"); // This gives us a cool logo
-const Choice = require("inquirer/lib/objects/choice");
 require("console.table");
 
 init();
 
-function start() {
-  const coolLogo = logo({ name: "Employee Tracker" }).render();
-  console.log(coolLogo);
-  mainPrompts();
-}
 function mainPrompts() {
   prompts ([
     {
