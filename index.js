66d9c0761cfff1f9651358f82e93c5f160cbd5f5
Alicia Vega <aliciavega@Alicias-MacBook-Pro.local>
2021-03-06 18:05:19 -0700

    updated schema file with tables

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const inquirer = require('inquirer')
+// Requiring everything at the top of the file
+const inquirer = require("inquirer");
+const db = require("./db");
+const logo = require("asciiart-logo"); // This gives us a cool logo
+require("console.table");
+
+init();
+
+function init() {
+  const coolLogo = logo({ name: "Employee Tracker" }).render();
+  console.log(coolLogo);
+  mainPrompts();
+}
+
+function mainPrompts() {
+
+}
